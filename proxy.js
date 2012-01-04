12d2ab67b0832a31df9bd2cf5a108507dc7215ba
Rob Cameron <rwcameron@gmail.com>
2012-01-03 17:00:36 -0800

    added in / redirect and /register redirect

--- proxy.js
+++ proxy.js
@@ -1,6 +1,7 @@
 var cluster = require('cluster');
 var http = require('http');
 var numCPUs = require('os').cpus().length;
+var fs = require('fs');
 
 if (cluster.isMaster) {
   // Fork workers.
@@ -18,9 +19,9 @@ if (cluster.isMaster) {
 	
 	var options = {
 	  https: {
-		key: fs.readFileSync('/root/ninjafaq.key', 'utf8'),
-		cert: fs.readFileSync('/root/ninjafaq.com.crt', 'utf8'),
-		ca: fs.readFileSync('/root/sf_bundle.crt','utf8')
+		key: fs.readFileSync('./keys/ninjafaq.com.key', 'utf8'),
+		cert: fs.readFileSync('./keys/ninjafaq.com.crt', 'utf8'),
+		ca: fs.readFileSync('./keys/sf_bundle.crt','utf8')
 	  }
 	};
 	
@@ -28,10 +29,29 @@ if (cluster.isMaster) {
 	// Create your proxy server
 	var proxy = new httpProxy.RoutingProxy();
 	https.createServer(options.https, function(req,res) {
-	  proxy.proxyRequest(req,res, {
-		host: 'localhost',
-		port: 5984
-	  })
+	  if (req.url == '/register') {
+	    console.log('reg');
+	    fs.readFile('./index.htm', function(error, content) {
+          if (error) {
+              res.writeHead(500);
+              res.end();
+          }
+          else {
+              res.writeHead(200, { 'Content-Type': 'text/html' });
+              res.end(content, 'utf-8');
+          }
+      });
+    } else if (req.url == '/') {
+      res.writeHead(302, {
+      'Location': 'https://www.ninjafaq.com/faqman/_design/FAQcouch/index.html'
+      });
+      res.end();
+	  } else {
+      proxy.proxyRequest(req,res, {
+        host: 'localhost',
+        port: 5984
+      });
+	  };
 	}).listen(443);
 	
 	http.createServer(function(req,res) {
